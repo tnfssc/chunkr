ddc87fb5a4541df3de9f8763ea6c9822c98c9c50
Akhilesh Sharma <sharmaninenine@gmail.com>
2024-10-13 00:49:40 +0000

    ppocr 2 onxx done

--- services/ocr_server/setup/main.py
+++ services/ocr_server/setup/main.py
@@ -5,10 +5,15 @@ import subprocess
 import tarfile
 
 MODEL_DIR = "../models"
+ONNX_DIR = "../onnx"
 
-def download_table_rec(force_download: bool):
-    url = "https://paddleocr.bj.bcebos.com/ppstructure/models/slanet/en_ppstructure_mobile_v2.0_SLANet_infer.tar"
-    model_name = "en_ppstructure_mobile_v2.0_SLANet_infer"
+models = {
+    "https://paddleocr.bj.bcebos.com/PP-OCRv3/english/en_PP-OCRv3_det_infer.tar",
+    "https://paddleocr.bj.bcebos.com/PP-OCRv3/english/en_PP-OCRv3_rec_infer.tar",
+    # "https://paddleocr.bj.bcebos.com/ppstructure/models/slanet/en_ppstructure_mobile_v2.0_SLANet_infer.tar"
+}
+
+def download_and_extract_model(force_download: bool, url: str, model_name: str) -> str:
     tar_filename = os.path.join(MODEL_DIR, f"{model_name}.tar")
     if not force_download and os.path.exists(tar_filename):
         return os.path.join(MODEL_DIR, model_name)
@@ -23,31 +28,31 @@ def download_table_rec(force_download: bool):
         tar.extractall(path=MODEL_DIR)
         
     os.remove(tar_filename)
-    
-    print("Table recognition model downloaded and extracted successfully.")
 
     return os.path.join(MODEL_DIR, model_name)
 
-def convert_to_onnx(model_path: str):
-    print(model_path)
+def convert_to_onnx(model_path: str, model_name: str):
     subprocess.run([
         "uv",
         "run",
         "paddle2onnx",
         "--model_dir", model_path,
-        "--model_filename", "model.pdmodel",
-        "--params_filename", "model.pdiparams",
-        "--save_file", os.path.join(model_path, "model.onnx")
+        "--model_filename", "inference.pdmodel",
+        "--params_filename", "inference.pdiparams",
+        "--save_file", os.path.join(ONNX_DIR, f"{model_name}.onnx")
     ])
 
-
 def main(force_download: bool):
-    model_path = download_table_rec(force_download)
-    convert_to_onnx(model_path)
+    for url in models:
+        model_name = os.path.splitext(url.split("/")[-1])[0]
+        print(f"Downloading and converting {model_name}...")
+        model_path = download_and_extract_model(force_download, url, model_name)
+        convert_to_onnx(model_path, model_name)
 
 if __name__ == "__main__":
     args = argparse.ArgumentParser()
     args.add_argument("--force_download", action="store_true")
     args = args.parse_args()
     os.makedirs(MODEL_DIR, exist_ok=True)
+    os.makedirs(ONNX_DIR, exist_ok=True)
     main(args.force_download)
